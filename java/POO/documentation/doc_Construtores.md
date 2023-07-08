03934db109bbcc48fef079c66744ab980d7d06b9
Lucas Davila <nightdavila@gmail.com>
2023-07-08 17:49:05 -0300

    Aprendendo o modulo Construtores, o app de produtos reformulad

---
+++ java/POO/documentation/doc_Construtores.md
@@ -0,0 +1,29 @@
+<h1>Construtor
+
+* É uma operação especial da classe, que executa no momento da instanciação do objeto.
+* Usos comuns:
+  * Iniciar valores dos atributos
+  * Permitir ou obrigar que o objeto receba dados / dependências no momento de sua instanciação (injenção de dependência)
+* Se um construtor customizado não for especificado, a classe disponibiliza o construto padrão:
+  Product p = new Product();
+* É possivel especificar mais de um construtor na mesma classe (sobrecarga)
+
+<h1>Problema exemplo
+
+![1688848038944](image/doc_Construtores/1688848038944.png)
+
+<h1>Proposta de melhoria
+
+Quando executamos o comando abaixo, instanciamos um produto **"product"** com seus atributos ''Vazios''":
+
+product = new Product(); ![1688848281874](image/doc_Construtores/1688848281874.png)
+
+
+Entretanto, faz sentido um produto que não tem nome? Faz sentido um produto que não tem preço?
+
+
+Com o intuito de evitar a existência de produtos sem nome e sem preço, é possível fazer com que seja "obrigatória" a iniciação desses valores?
+
+<h2>Apos modificado
+
+![1688849273731](image/doc_Construtores/1688849273731.png)
---
+++ java/POO/src/constructor/entities/ProductEntity.java
@@ -0,0 +1,55 @@
+package constructor.entities;
+
+import java.util.Scanner;
+
+public class ProductEntity {
+    Scanner sc = new Scanner(System.in);
+    private String name;
+    private double price;
+    private int quantity;
+
+    public ProductEntity(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    public void produtoPrint() {
+        System.out.print("Product is: ");
+        System.out.println(name + ", "+ price + ", "+ quantity);
+    }
+
+    public String setName() {
+        return name = sc.next();
+    }
+
+    public double totalValueInStock() {
+        return quantity * price;
+    }
+
+    public void addProducts(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity) {
+        this.quantity = this.quantity - quantity;
+        System.out.println("A quantidade de produtos agora é -> " + this.quantity);
+    }
+
+    public double setPrice() {
+        return price = sc.nextDouble();
+    }
+
+    public int setQuantity() {
+        return quantity = sc.nextInt();
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
+}
