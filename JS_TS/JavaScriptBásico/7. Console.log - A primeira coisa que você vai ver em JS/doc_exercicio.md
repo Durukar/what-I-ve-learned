e14d5f91f14c1d94a64ac29a9b3460cefde9344c
Durukar <nightdavila@gmail.com>
2023-07-10 16:23:32 -0300

    Estou iniciando o aprendizado em JS para migrar e me especializar em TS.

---
+++ JS_TS/JavaScriptBásico/7. Console.log - A primeira coisa que você vai ver em JS/doc_exercicio.md
@@ -0,0 +1,3 @@
+<h1> Exercicio de fixacao
+
+Adicione no js o texto "Meu nome é 'NOME'. Estou aprendendo JavaScript as 10 da manha.
---
+++ JS_TS/JavaScriptBásico/7. Console.log - A primeira coisa que você vai ver em JS/exercicio.js
@@ -0,0 +1,3 @@
+let nome = 'Lucas';
+let hora = 10;
+console.log(`Meu nome é ${nome}. Estou aprendendo JavaScript às ${hora} da manhã`);
