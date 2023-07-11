bff9b4995e4205f1f599bec4055629b9987f4bd6
Durukar <nightdavila@gmail.com>
2023-07-11 10:40:34 -0300

    Varaveis com let

---
+++ JS_TS/JavaScriptBásico/13. Variáveis com let/index.js
@@ -0,0 +1,8 @@
+let nomeMasculino = 'João';
+let nomeFeminino = 'Maria';
+
+console.log(`${nomeMasculino} nasceu em 1984.`);
+console.log(`Em 2000 ${nomeMasculino} conheceu ${nomeFeminino}.`);
+console.log(`${nomeMasculino} casou-se com ${nomeFeminino} em 2012.`);
+console.log(`${nomeFeminino} teve 1 filho com ${nomeMasculino} em 2015.`);
+console.log(`O filho de ${nomeMasculino} se chama Eduardo.`);
