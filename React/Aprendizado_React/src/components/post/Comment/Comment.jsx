a3b8bd3dd71b8f0e55570072e1b855ea473c6a90
Lucas Davila <nightdavila@gmail.com>
2023-07-15 13:29:04 -0300

    Criado o component Comment, porem sem estilizaçao

---
+++ React/Aprendizado_React/src/components/post/Comment/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/durukar.png"/>
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Lucas Davila</strong>
+              <time title="15 de Julho ás 10:09" dateTime="2023-15-07">Cerca de 1h atrás</time>
+            </div>
+
+            <button tittle="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>O problema esta no item X do connector!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
