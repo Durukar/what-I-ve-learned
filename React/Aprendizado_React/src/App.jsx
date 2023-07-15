3eb9f9bb80e8cf765a0cc9a35a3e440ea4561f2f
Lucas Davila <nightdavila@gmail.com>
2023-07-15 02:44:27 -0300

    Sidebar criado e suas estilizaçoes

--- React/Aprendizado_React/src/App.jsx
+++ React/Aprendizado_React/src/App.jsx
@@ -4,18 +4,26 @@ import './global.css';
 import styles from './App.module.css'
 import { Sidebar } from './components/sidebar/Sidebar';
 
+import { Post } from './Post';
+
 export function App() {
   return (
     <div>
       <Header />
 
-      <div classNamer={styles.wrapper} >
+      <div className={styles.wrapper} >
         <Sidebar />
-        
         <main>
+          <Post
+            author='Lucas Davila' 
+            content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quae eum, vitae voluptatem expedita ipsa ut explicabo officiis fuga incidunt nobis ducimus odio praesentium! Exercitationem sunt cum eaque. Est, eligendi.'
+          />
+          <Post
+            author='Joao Cerafim' 
+            content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quae eum, vitae voluptatem expedita ipsa ut explicabo officiis fuga incidunt nobis ducimus odio praesentium! Exercitationem sunt cum eaque. Est, eligendi.'
+          />
         </main>
       </div>
     </div>
   )
 }
-
--- React/Aprendizado_React/src/components/sidebar/Sidebar.jsx
+++ React/Aprendizado_React/src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import stylesSidebar from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -7,12 +9,14 @@ export function Sidebar() {
         className={stylesSidebar.cover} 
         src="https://images.unsplash.com/photo-1605101479435-005f9c563944?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" />
 
-      <div className={stylesSidebar.Profile}>
+      <div className={stylesSidebar.profile}>
+        <img className={stylesSidebar.avatar} src="https://avatars.githubusercontent.com/u/9040316?v=4" />
         <strong>Lucas Davila</strong>
-        <span>Web Developer</span>
+        <span>Snews Developer</span>
       </div>
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
