acba6ca7a255e0e60f5442a1458cc1290b34331d
Lucas Davila <nightdavila@gmail.com>
2023-07-21 01:02:54 -0300

    Criado o projeto e criado os primeiros componentes

---
+++ JS_TS/projetos_React/dio-bank/src/App.tsx
@@ -0,0 +1,11 @@
+import { Card } from './components/Card'
+import { Layout } from './components/Layout';
+export function App() {
+  return (
+    <Layout>
+      Dio Bank
+    </Layout>
+  );
+}
+
+export default App;
